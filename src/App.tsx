52885ae0ed2d963eca054ed93ba04e267da00d1d
gusflopes <gusflopes86@gmail.com>
2020-05-22 15:14:08 -0400

    feat: finished menu --need icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React, { ComponentProps, useState } from 'react';
+import React, { ComponentProps, useState, useRef } from 'react';
+import { CSSTransition } from 'react-transition-group'
 
 function App() {
   return (
@@ -6,6 +7,7 @@ function App() {
       <NavItem icon="😎" />
       <NavItem icon="🤯" />
       <NavItem icon="🙃" />
+
       <NavItem icon="🤮">
         <DropdownMenu />
       </NavItem>
@@ -27,11 +29,13 @@ interface NavItemProps extends ComponentProps<any> {
 
 const NavItem: React.FC<NavItemProps> = (props) => {
   const [open, setOpen] = useState(false);
+
   return (
     <li className="nav-item">
       <a href="#hello" className="icon-button" onClick={() => setOpen(!open)}>
         {props.icon}
       </a>
+
       {open && props.children}
     </li>
   )
@@ -40,23 +44,84 @@ const NavItem: React.FC<NavItemProps> = (props) => {
 interface DropdownItemProps extends ComponentProps<any> {
   leftIcon?: string;
   rightIcon?: string;
+  goToMenu?: string;
 }
-const DropdownMenu = () => {
+
+const DropdownMenu: React.FC = () => {
+  const [activeMenu, setActiveMenu] = useState('main')
+  const [menuHeight, setMenuHeight] = useState();
+  const dropdownRef = useRef(null)
+
+  const calcHeight = (el: any) => {
+    const height = el.offsetHeight
+    setMenuHeight(height)
+  }
+
   const DropdownItem: React.FC<DropdownItemProps> = (props) => {
     return (
-      <a href="#hello" className="menu-item">
+      <a href="#hello" className="menu-item" onClick={() => props.goToMenu && setActiveMenu(props.goToMenu)}>
         <span className="icon-button">{props.leftIcon}</span>
         {props.children}
         <span className="icon-right">{props.rightIcon}</span>
       </a>
     )
-
   }
+
   return (
-    <div className="dropdown">
-      <DropdownItem>My Profile</DropdownItem>
-      <DropdownItem leftIcon="👹" rightIcon="👽">My Profile</DropdownItem>
-      <DropdownItem>My Profile</DropdownItem>
+    <div className="dropdown" style={{ height: menuHeight }} ref={dropdownRef} >
+
+      <CSSTransition
+        in={activeMenu === 'main'}
+        timeout={500}
+        classNames="menu-primary"
+        unmountOnExit
+        onEnter={calcHeight}
+        addEndListener={() => { }}
+      >
+        <div className="menu">
+          <DropdownItem leftIcon="👹" rightIcon="👽">My Profile</DropdownItem>
+          <DropdownItem goToMenu="settings">Settings</DropdownItem>
+          <DropdownItem goToMenu="animals">Animals</DropdownItem>
+          <DropdownItem>My Profile</DropdownItem>
+          <DropdownItem>My Profile</DropdownItem>
+          <DropdownItem>My Profile</DropdownItem>
+          <DropdownItem>My Profile</DropdownItem>
+        </div>
+      </CSSTransition>
+
+      <CSSTransition
+        in={activeMenu === 'settings'}
+        timeout={500}
+        classNames="menu-secondary"
+        unmountOnExit
+        onEnter={calcHeight}
+        addEndListener={() => { }}
+      >
+        <div className="menu">
+          <DropdownItem goToMenu="main" leftIcon="‹">
+            <h2>Settings</h2>
+          </DropdownItem>
+          <DropdownItem leftIcon="👹" rightIcon="👽">My Profile</DropdownItem>
+          <DropdownItem>Qualquer</DropdownItem>
+        </div>
+      </CSSTransition>
+
+      <CSSTransition
+        in={activeMenu === 'animals'}
+        timeout={500}
+        classNames="menu-secondary"
+        unmountOnExit
+        onEnter={calcHeight}>
+        <div className="menu">
+          <DropdownItem goToMenu="main" leftIcon="‹">
+            <h2>Animals</h2>
+          </DropdownItem>
+          <DropdownItem leftIcon="🦘">Kangaroo</DropdownItem>
+          <DropdownItem leftIcon="🐸">Frog</DropdownItem>
+          <DropdownItem leftIcon="🦋">Horse?</DropdownItem>
+          <DropdownItem leftIcon="🦔">Hedgehog</DropdownItem>
+        </div>
+      </CSSTransition>
     </div>
   )
 }
